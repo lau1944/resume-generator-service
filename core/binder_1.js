8229e74a6f964863566e39b9b892ea1b9a439714
lau1944 <a954723219@gmail.com>
2023-02-02 18:05:28 -0500

    feat: separate bind function

---
+++ core/binder_1.js
@@ -0,0 +1,166 @@
+const { parseProfileItem, parseLinkView, parseLinkItem, parseSkillView, parseSkillItem, parseLangView, parseLangItem, parseEducationView, parseEduItem, parseWorkView, parseWorkItem, parseProjectView, parseProjectItem } = require('../core/parser_1')
+/**
+ *
+ * @param {Node} Html root node
+ * @param {Resume} resume detail
+ * @returns
+ */
+function bind (root, data) {
+  // bind data into profile view
+  bindProfile(data.profile, root)
+
+  // bind link into profie
+  bindLinks(data.links, root)
+
+  // bind skills into skill view
+  bindSkills(data.skills, root)
+
+  // bind lang name into lang view
+  bindLangs(data.langs, root)
+
+  // bind intro
+  bindIntro(root, data.introduction)
+
+  // bind education
+  bindEducation(root, data.educations)
+
+  // bind work
+  bindWork(root, data.works)
+
+  // bind project
+  bindProject(root, data.projects)
+
+  return root.toString()
+}
+
+function bindProject (root, projects) {
+  if (!projects || projects.length === 0) {
+    return
+  }
+
+  const eduView = root.querySelector('#des-list')
+  eduView.appendChild(parseProjectView())
+
+  const eduList = root.querySelector('#project-list')
+  for (const project of projects) {
+    eduList.appendChild(parseProjectItem(project))
+  }
+}
+
+// bind work into view
+function bindWork (root, works) {
+  if (!works || works.length === 0) {
+    return
+  }
+
+  const eduView = root.querySelector('#des-list')
+  eduView.appendChild(parseWorkView())
+
+  const eduList = root.querySelector('#work-list')
+  for (const work of works) {
+    eduList.appendChild(parseWorkItem(work))
+  }
+}
+
+// bind education to view
+function bindEducation (root, edus) {
+  if (!edus || edus.length === 0) {
+    return
+  }
+  const eduView = root.querySelector('#des-list')
+  eduView.appendChild(parseEducationView())
+
+  const eduList = root.querySelector('#school-list')
+  for (const edu of edus) {
+    eduList.appendChild(parseEduItem({ name: edu.name, startTime: edu.startTime, endTime: edu.endTime, location: edu.location, content: edu.content, degree: edu.degree }))
+  }
+}
+
+// bind intro to view
+function bindIntro (root, intro) {
+  const element = root.querySelector('#introduction')
+  element.set_content(intro)
+}
+
+// bind user profile details to html
+function bindProfile (profile, root) {
+  const {
+    firstName,
+    lastName,
+    role,
+    avatar,
+    email,
+    country,
+    phoneNumber,
+    city
+  } = profile
+  root.querySelector('#first-name').set_content(firstName)
+  root.querySelector('#last-name').set_content(lastName)
+  root.querySelector('#role').set_content(role)
+  if (avatar) {
+    root.querySelector('#avatar').setAttribute('src', avatar)
+  }
+  const details = root.querySelector('#profile-list')
+  if (email) {
+    details.appendChild(parseProfileItem('EMAIL', email))
+  }
+
+  if (country) {
+    details.appendChild(parseProfileItem('COUNTRY', country))
+  }
+
+  if (city) {
+    details.appendChild(parseProfileItem('CITY', city))
+  }
+
+  if (phoneNumber) {
+    details.appendChild(parseProfileItem('PHONE NUMBER', phoneNumber))
+  }
+}
+
+function bindLinks (links, root) {
+  if (!links || links.length === 0) {
+    return
+  }
+
+  const linkElement = root.querySelector('#links')
+  linkElement.appendChild(parseLinkView())
+
+  const linkList = root.querySelector('#link-list')
+  for (const e of links) {
+    const { type, link } = e
+    linkList.appendChild(parseLinkItem(type, link))
+  }
+}
+
+function bindSkills (skills, root) {
+  if (!skills || skills.length === 0) {
+    return
+  }
+
+  const skillElement = root.querySelector('#skills')
+  skillElement.appendChild(parseSkillView())
+
+  const skillList = root.querySelector('#skill-list')
+  for (const e of skills) {
+    const { name, level } = e
+    skillList.appendChild(parseSkillItem(name, level))
+  }
+}
+
+function bindLangs (langs, root) {
+  if (!langs || langs.length === 0) {
+    return
+  }
+
+  const skillElement = root.querySelector('#language')
+  skillElement.appendChild(parseLangView())
+
+  const skillList = root.querySelector('#lang-list')
+  for (const e of langs) {
+    const { name, level } = e
+    skillList.appendChild(parseLangItem(name, level))
+  }
+}
+
+module.exports = bind
---
+++ core/parser_1.js
@@ -0,0 +1,213 @@
+const HTMLParser = require('node-html-parser')
+
+const parseProjectView = () => {
+  return HTMLParser.parse(`
+    <div id="project">
+      <h2>Projects</h2>
+      <div class="title-divide"></div>
+
+      <ul id="project-list"></ul>
+    </div>
+  `)
+}
+
+const parseProjectItem = ({ title, link, content, startTime, endTime }) => {
+  return HTMLParser.parse(`
+    <li id="project-item">
+      <div class="header-layer">
+        <div id="project-title">${title || ''}</div>
+        <div class="project-extra">
+          <a
+            id="project-link"
+            href="${link || ''}"
+            target="_blank"
+            rel="noreferrer"
+            >Project link</a>
+          <div class="horizon">
+            <div>${startTime || ''}</div>
+            <div class="indicator"> - </div>
+            <div>${endTime || ''}</div>
+          </div>
+        </div>
+      </div>
+
+      <div id="project-content">${content || ''}</div>
+    </li>
+  `)
+}
+
+const parseWorkView = () => {
+  return HTMLParser.parse(`
+  <div id="experience">
+    <h2>Work Experience</h2>
+    <div class="title-divide"></div>
+
+    <ul id="work-list"></ul>
+
+    <hr class="solid" />
+  </div>
+  `)
+}
+
+const parseWorkItem = ({ name, role, location, startTime, endTime, description }) => {
+  return HTMLParser.parse(`
+    <li>
+      <div class="title">
+        <div id="company-name">${role || ''}, ${name || ''}</div>
+        <div>
+          <div id="company-location">${location || ''}</div>
+          <div id="company-time">
+            <div>${startTime || ''}</div>
+            <div class="indicator"> - </div>
+            <div>${endTime || ''}</div>
+          </div>
+        </div>
+      </div>
+
+      <div id="work">
+        ${description || ''}
+      </div>
+    </li>
+  `)
+}
+
+const parseEducationView = () => {
+  return HTMLParser.parse(`
+    <div id="education">
+      <h2>Education</h2>
+      <div class="title-divide"></div>
+
+      <ul id="school-list" class="school">
+      </ul>
+    </div>
+    <hr class="solid" />
+  `)
+}
+
+const parseEduItem = ({ name, startTime, endTime, location, content, degree }) => {
+  return HTMLParser.parse(`
+    <li class="school-item">
+      <div>
+        <h3 id="university">${name || ''}</h3>
+        <div id="school-info">
+          <div id="school-degree">${degree || ''}</div>
+          <div id="school-time">
+            <div>${startTime || ''}</div>
+            <div class="indicator"> - </div>
+            <div>${endTime || ''}</div>
+          </div>
+        </div>
+        <div id="school-text">${content || ''}</div>
+      </div>
+      <div>
+        <div id="school-location">${location || ''}</div>
+      </div>
+    </li>
+  `)
+}
+
+const parseProfileItem = (type, content) => {
+  return HTMLParser.parse(`
+  <li class="profile-item">
+    <div>${type || ''}</div>
+    <div class="profile-txt">${content || ''}</div>
+  </li>`)
+}
+
+const parseLinkView = () => {
+  return HTMLParser.parse(`
+    <h2>LINKS</h2>
+    <div class="title-divide"></div>
+    <ul id="link-list"></ul>
+  `)
+}
+
+const parseLinkItem = (type, link) => {
+  if (!type || !link) {
+    return
+  }
+
+  return HTMLParser.parse(`
+    <li class="profile-item">
+      <a class="link-txt" href="${link}">${type}</a>
+    </li>
+  `)
+}
+
+const parseSkillView = () => {
+  return HTMLParser.parse(`
+    <h2>SKILLS</h2>
+    <div class="title-divide"></div>
+    <ul id="skill-list"></ul>
+  `)
+}
+
+const parseSkillItem = (name, level) => {
+  if (level && (level < 1 || level > 5)) {
+    throw new Error('level should not smaller than 1 or larger than 5')
+  }
+
+  let levelView = ''
+  if (level) {
+    levelView = `
+    <div id="bar">
+      <div class="bar-container">
+        <div class="bar-${level}"></div>
+      </div>
+    </div>`
+  }
+
+  return HTMLParser.parse(`
+      <li class="skill-item">
+      <div id="skill-name">${name || ''}</div>
+      ${levelView}
+    </li>
+  `)
+}
+
+const parseLangView = () => {
+  return HTMLParser.parse(`
+    <h2>LANGUAGES</h2>
+    <div class="title-divide"></div>
+    <ul id="lang-list"></ul>
+  `)
+}
+
+const parseLangItem = (name, level) => {
+  if (level && (level < 1 || level > 5)) {
+    throw new Error('level should not smaller than 1 or larger than 5')
+  }
+
+  let levelView = ''
+  if (level) {
+    levelView = `
+    <div id="bar">
+      <div class="bar-container">
+        <div class="bar-${level}"></div>
+      </div>
+    </div>`
+  }
+
+  return HTMLParser.parse(`
+      <li class="lang-item">
+      <div id="lang-name">${name}</div>
+      ${levelView}
+    </li>
+  `)
+}
+
+module.exports = {
+  parseProfileItem,
+  parseLinkView,
+  parseLinkItem,
+  parseSkillView,
+  parseSkillItem,
+  parseLangView,
+  parseLangItem,
+  parseEducationView,
+  parseEduItem,
+  parseWorkView,
+  parseWorkItem,
+  parseProjectView,
+  parseProjectItem
+}
