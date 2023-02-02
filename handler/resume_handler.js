7304f6ce916f7b7c13464748bb53da372997f01f
lau1944 <a954723219@gmail.com>
2023-02-02 17:58:22 -0500

    feat: bind function map

--- handler/resume_handler.js
+++ handler/resume_handler.js
@@ -1,13 +1,26 @@
 /* eslint-disable space-before-function-paren */
 const { parse, parseProfileItem, parseLinkView, parseLinkItem, parseSkillView, parseSkillItem, parseLangView, parseLangItem, parseEducationView, parseEduItem, parseWorkView, parseWorkItem, parseProjectView, parseProjectItem } = require('../core/html_parser')
 
+const CLASSIC_RESUME_ID = 'classic'
+
+const bindMap = {
+  [CLASSIC_RESUME_ID]: bind
+}
+
 const handleAll = async (data, callback) => {
   const details = data.request
   const resumeId = details.resumeId
 
   try {
+    const bindFunc = bindMap[resumeId]
+
+    if (!bindFunc) {
+      callback(new Error('Cannot find this resume id'), { code: 2, status: 'failed', body: null })
+      return
+    }
+
     const root = await parse(resumeId)
-    callback(null, { code: 1, status: 'success', body: bind(root, details) })
+    callback(null, { code: 1, status: 'success', body: bindFunc(root, details) })
   } catch (e) {
     callback(e, { code: 2, status: 'failed', body: null })
   }
